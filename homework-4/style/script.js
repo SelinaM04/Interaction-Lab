2d6c991b6cd0a5bcc07a9a92569d20da9eef0f08
SelinaM04 <may162@newschool.edu>
2024-02-29 21:45:48 -0500

    2.29

---
+++ homework-4/style/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.artwork').forEach(artwork => {
+  artwork.addEventListener('click', function() {
+    const infoDiv = this.querySelector('.artwork-info'); 
+    const coverDiv = this.querySelector('.artwork-cover');
+    infoDiv.classList.toggle('is-visible');
+    coverDiv.style.opacity = coverDiv.style.opacity === '0' ? '1' : '0';
+  });
+});
